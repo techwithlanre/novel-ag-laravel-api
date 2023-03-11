b9088685446836eb472b05908c35c6cb8e5ca800
Jamiu Olanrewaju <96052702+techwithlanre@users.noreply.github.com>
2023-03-11 12:04:40 +0100

    database migration added

--- app/Http/Controllers/Auth/FarmerRegisterController.php
+++ app/Http/Controllers/Auth/FarmerRegisterController.php
@@ -4,15 +4,36 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FarmerRegisterRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class FarmerRegisterController extends Controller
 {
     /**
      * Handle the incoming request.
+     * @param FarmerRegisterRequest $request
+     * @return \Illuminate\Http\Response
      */
     public function __invoke(FarmerRegisterRequest $request)
     {
-        //
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'state_id' => $request->state_id,
+            'local_government_id' => $request->local_government_id,
+            'ward_id' => $request->ward_id,
+            'accept_terms' => $request->accept_terms
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return response()->noContent();
     }
 }
--- app/Http/Requests/FarmerRegisterRequest.php
+++ app/Http/Requests/FarmerRegisterRequest.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
 
 class FarmerRegisterRequest extends FormRequest
 {
@@ -17,12 +21,70 @@ class FarmerRegisterRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array
      */
     public function rules(): array
     {
         return [
-            'first_name'=>['required'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['string', 'email', 'max:255', 'unique:'.User::class],
+            'state_id' => ['required','int'],
+            'local_government_id' => ['required','int'],
+            'ward_id' => ['int'],
+            'accept_terms'=>['required', 'int']
         ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'first_name.required' => 'First name is required',
+            'last_name.required' => 'Last name is required',
+            'email.required' => 'Email is required',
+            'state_id.required' => 'Select a state.',
+            'local_government_id.required' => 'Select a local government',
+            'accept_terms.required' => 'Accept terms & conditions',
+        ];
+    }
+
+    /**
+     *  Filters to be applied to the input.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            //'email' => 'trim|lowercase',
+            //'first_name' => 'trim|capitalize|escape',
+            //'last_name' => 'trim|capitalize|escape',
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            //'email' => 'email address',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'validation-errors',
+            'data'      => $validator->errors()->all()
+        ], Response::HTTP_BAD_REQUEST));
     }
 }
